c33ce19137a866ea3f5cf8ef5de2d13b4a608433
Raymond Guo <qingnantech@gmail.com>
2024-11-13 18:13:14 +0800

    chore: fix cloudflare compat issue

--- app.config.ts
+++ app.config.ts
@@ -8,8 +8,10 @@ export default defineConfig({
   server: {
     preset: "cloudflare-pages",
 
+    // We will need to enable CF Pages node compatiblity
+    // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
     rollupConfig: {
-      external: ["node:async_hooks"],
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
     },
   },
   extensions: ["mdx", "md"],
